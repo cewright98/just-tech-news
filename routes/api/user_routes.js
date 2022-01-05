bf1d8ef39b75cc996c810d5d3a3c936c68093ea9
Claire <cewright1998@gmail.com>
2022-01-05 14:23:20 -0500

    stuff

--- routes/api/user_routes.js
+++ routes/api/user_routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -15,14 +15,26 @@ router.get('/', (req, res) => {
         });
 });
 
-// GET /pi/users/1
+// GET /api/users/1
 router.get('/:id', (req, res) => {
     // equivalent to SELECT * FROM users WHERE id = 1
     User.findOne({
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
     })
     .then(dbUserData => {
         if (!dbUserData) {
