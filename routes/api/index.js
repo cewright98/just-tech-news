be7b5f009edd1188caec3b34c0690634490cd23e
Claire <cewright1998@gmail.com>
2022-01-05 13:40:43 -0500

    created all post routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const useRoutes = require('./user_routes');
+const postRoutes = require('./post_routes');
 
 router.use('/users', useRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
